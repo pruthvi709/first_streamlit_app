224cfb786f38042c22cce71478e0ca9d406e54d8
pruthvi709 <114474349+pruthvi709@users.noreply.github.com>
2022-10-12 09:44:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,10 +52,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
